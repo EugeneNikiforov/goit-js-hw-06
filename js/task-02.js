720e3c440af6d76c734c27467e5d651c27b098cb
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2022-12-26 00:02:36 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+// const pushIngredients = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
+const pushIngredients = ingredients.map(item => {
+  const items = document.createElement(`li`)
+  items.classList.add(`item`)
+  items.textContent = item
+  return items
+})
+// ingredientsList.innerHTML = pushIngredients;
+ingredientsList.append(...pushIngredients);
