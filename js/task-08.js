762c82d9da00b511e6f31eda3dc1888aaae16dfd
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2022-12-26 20:29:33 +0200

    Summary 08-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener(`submit`, formSubmit);
+function formSubmit(event) {
+    event.preventDefault();
+    const formInput = event.currentTarget.elements;
+    const mail = formInput.email.value;
+    const password = formInput.password.value;
+    const result = {
+        mail,
+        password,
+    };
+    if (mail && password !== ``) {
+        console.log(result);
+        form.reset();     
+    } else {
+        alert(`Fill in all fields of the form`);
+    };
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bgndColor = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+btnChangeColor.addEventListener(`click`, colorFoo);
+
+function colorFoo() {
+  const changeColor = getRandomHexColor();
+  spanColor.textContent = changeColor;
+  console.log(bgndColor.style.backgroundColor = changeColor);
+};
